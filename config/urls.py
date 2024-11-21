059be506c885207283daed0da13dc3399f186a5f
kkalb <kevinkalb17@googlemail.com>
2024-11-21 13:15:10 +0000

    intermediate commit

--- config/urls.py
+++ config/urls.py
@@ -8,19 +8,17 @@ from django.views import defaults as default_views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    path(
-        "about/",
-        TemplateView.as_view(template_name="pages/about.html"),
-        name="about",
-    ),
+    path("", TemplateView.as_view(template_name="pages/text.html"), name="text"),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
     path("users/", include("speech_to_text.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
-    # ...
+    path(
+        "text/",
+        include("speech_to_text.main.urls", namespace="text"),
+    ),
     # Media files
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
--- speech_to_text/main/models.py
+++ speech_to_text/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Text(models.Model):
+    data = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.data
+
+    def get_absolute_url(self):
+        return reverse("text:list")
